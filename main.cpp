05bf318fe9f5374c5cf6aac63fa5e7679a730a87
mnasafi <mnasafi@mail.csuchico.edu>
2018-03-28 18:24:04 -0700

    compilation errors, something to do with char assignment; need to debug.

--- main.cpp
+++ main.cpp
@@ -1,22 +1,54 @@
-//Author:
+//Author: Mustafa Nasafi
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <stdlib.h>
 
 using namespace std;
 
 int main()
 {
+
+  string word;
+  vector <string> v;
   ifstream in("example.txt");
   char first = ' ', last = ' ', current;
+  
 
   //Read from the file opened by in, and the first letter (either upper or
   //lower case) should be assigned to first and the last letter (again in
   //either case) from the file to the variable last
 
 
+  // check for error opening file
+  if ( in.fail() )
+  {
+      cerr << "Error opening file" << endl;
+      exit(1);
+  }
+
+  while( !in.eof() )
+  {
+      in >> word;
+      v.push_back( word );
+
+
+  } // end while
+
+  
+  // return first character from vector v
+  first = v.at(0);
+
+  // get last character from vector v
+  last = v [ v.size() -1 ];
+
 
   //Then, finally, this displays the censored version:
-  cout<<first<<"**"<<last<<endl;
+  cout << first << "**" << last << endl;
+
+  // sample output
+  // John is awesome ---> J**e
 
   return 0;
-}
+
+} // end main
